2f5977b5901f9c44b8dcad369f083098f074003e
Daniel-Alegria <libertatia@protonmail.com>
2018-11-19 12:59:30 -0800

    fixed

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-#! /usr/bin/env node
-const variables = require("./variables");
+const fs = require("fs-extra");
 const concat = require("concat");
-const path = require("path");
-const fs = require("fs");
 const replace = require("replace-in-file");
+const path = require("path");
+const variables = require("./variables.js");
 
-// for (var key in variables) {
-//   console.log(key);
-// }
-
-function gatherCSS(startPath, filter) {
-  var results = [];
-
-  if (!fs.existsSync(startPath)) {
-    console.log("no dir ", startPath);
-    return;
-  }
-
-  var files = fs.readdirSync(startPath);
+const newFile = __dirname + "/style.css";
+function findFiles() {
+  let results = [];
+  let files = fs.readdirSync(__dirname);
   for (var i = 0; i < files.length; i++) {
-    var filename = path.join(startPath, files[i]);
+    var filename = path.join(__dirname, files[i]);
     var stat = fs.lstatSync(filename);
-    if (stat.isDirectory()) {
-      results = results.concat(gatherCSS(filename, filter)); //recurse
-    } else if (filename.indexOf(filter) >= 0) {
+    if (
+      filename.indexOf(".css") >= 0 &&
+      filename !== __dirname + "/style.css"
+    ) {
       console.log("-- found: ", filename);
       results.push(filename);
     }
@@ -31,36 +22,18 @@ function gatherCSS(startPath, filter) {
   return results;
 }
 
-function addVariables(file) {
-  var options = {
-    files: file,
-    from: [],
-    to: []
-  };
+let files = findFiles();
 
-  for (var i = 0; i < Object.keys(variables).length; i++) {
-    options.from.push(`(` + "\\" + `${Object.keys(variables)[i]})/g`);
-  }
-
-  for (var i = 0; i < Object.values(variables).length; i++) {
-    options.to.push(Object.values(variables)[i]);
-  }
-
-  console.log(options);
-
-  replace(options, (error, changes) => {
-    if (error) {
-      return console.error("Error occurred:", error);
-    }
-    console.log("Modified files:", changes.join(", "));
-  });
-}
-
-function concatCSS(files) {
-  concat(files, "./style.css");
-  addVariables("./style.css");
-}
-
-concatCSS(gatherCSS("./", ".css"));
-
-// for (var i = 0; i < variables.length; console.log(variables[i]));
+concat(files, newFile).then(
+  replace({
+    files: newFile,
+    from: Object.keys(variables),
+    to: Object.values(variables)
+  })
+    .then(changes => {
+      console.log("Modified files:", changes.join(", "));
+    })
+    .catch(error => {
+      console.error("Error occurred:", error);
+    })
+);
